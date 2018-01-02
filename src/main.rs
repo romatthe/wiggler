a1d2747d4363995a0000dd04902d2efe5cc3f6d5
romatthe <robin.mattheussen@gmail.com>
2018-01-02 18:17:36 +0100

    Set up channels/timers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::sync::mpsc::sync_channel;
+use std::thread;
+use std::time::Duration;
+
+struct WiggleRequest;
+
 fn main() {
-    println!("Hello, world!");
+
+    let (sender, receiver) = sync_channel(1);
+
+    thread::spawn(move || {
+        loop {
+            thread::sleep(Duration::from_secs(5));
+            sender.send(WiggleRequest).unwrap();
+        }
+    });
+
+    loop {
+        match receiver.recv() {
+            Ok(WiggleRequest)   => println!("Wiggle Wiggle Wiggle!"),
+            Err(_)              => println!("Something went wrong while wiggling!")
+        }
+    }
+
 }
